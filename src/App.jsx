aae30188e5a9eb48c4bbc81cf90db4f1b16d95b2
Usmaniac2003 <usmancodepro@gmail.com>
2024-07-03 04:12:11 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+const URL="https://taskwhiz-266479ed8de8.herokuapp.com/";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {URL}
+       </>
+  )
+}
+
+export default App
