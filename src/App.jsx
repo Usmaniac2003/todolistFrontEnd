3f004b89ad1399fe47a4ef38889710c129c66a6c
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2024-08-10 19:02:18 +0500

    Login and Signup Page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react'
 import "./Styles/root.css"
-import { ArchiveList, EditProfilePage,PinnerList, TrashList } from './Pages';
+import { ArchiveList, EditProfilePage,PinnerList, TrashList,Login,Home,Lists, Signup } from './Pages/index.js';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import Home from './Pages/Home';
-import Lists from './Pages/Lists';
 
 const URL="https://taskwhiz-266479ed8de8.herokuapp.com/";
 
@@ -13,7 +11,9 @@ function App() {
 
   return (
     <>
-    <Lists></Lists>
+    {/* <Lists></Lists> */}
+    {/* <Login/> */}
+    <Signup/>
    </>
   )
 }
